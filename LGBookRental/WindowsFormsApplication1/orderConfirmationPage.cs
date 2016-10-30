d90df2a20acd943d6fdbc2c97a1b1c4bd9f6549a
Renick <renickdp@hotmail.com>
2016-10-29 22:38:39 -0400

    added order number writing to DB

--- LGBookRental/WindowsFormsApplication1/orderConfirmationPage.cs
+++ LGBookRental/WindowsFormsApplication1/orderConfirmationPage.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
+
 
 namespace LGBookRentals
 {
@@ -31,6 +33,37 @@ namespace LGBookRentals
                             + currentOrder.shippingAddress
                             + "\n Credit Card Number: "
                             + currentOrder.displayCreditCardNumber);
+
+
+            System.Data.OleDb.OleDbConnection conn = new System.Data.OleDb.OleDbConnection();
+
+            //MAKE SURE TO CHANGE THE DB LOCATION TO YOUR DIRECTORY
+            conn.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;" +
+        @"Data source= C:\Users\Masters PC\Desktop\SSE657 Object oriented design\LGBookRental\LGBookRental\WindowsFormsApplication1\db.mdb";
+
+            try
+            {
+                conn.Open();
+
+                String my_querry = "INSERT INTO orders (ordernumber) VALUES('"+123456+"')";
+
+                OleDbCommand cmd = new OleDbCommand(my_querry, conn);
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("order placed successfuly...!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed due to" + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+
+
+
         }
 
         private void goBackBtn_Click(object sender, EventArgs e)
